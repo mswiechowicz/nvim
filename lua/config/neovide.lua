7547872662b183d23a91936635e4e093d917fe69
mswiechowicz <mswiechowicz97@gmail.com>
2024-04-09 21:38:49 +0200

    fix previous commit

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-  vim.o.guifont = "FiraCode Nerd Font Mono:h12"
+  vim.o.guifont = "FiraCode Nerd Font Mono:h11"
   vim.keymap.set("n", "<D-s>", ":w<CR>")     -- Save
   vim.keymap.set("v", "<D-c>", '"+y')        -- Copy
   vim.keymap.set("n", "<D-v>", '"+P')        -- Paste normal mode
